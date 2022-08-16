9c1c8e8ed5dd23d47a133e388d6d6f42c8b44d3d
Kartik-sharma80 <49423167+Kartik-sharma80@users.noreply.github.com>
2022-08-16 18:40:48 +0530

    Delete result directory

--- result/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='home'),
-    path('result', views.result, name='result'),
-    path('student', views.student, name='student')
-]
