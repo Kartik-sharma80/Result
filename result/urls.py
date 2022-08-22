b8d47db1ea0d7eeafeccceec24f54035d60a8626
Kartik-sharma80 <49423167+Kartik-sharma80@users.noreply.github.com>
2022-08-22 14:32:44 +0530

    Update urls.py

--- result/urls.py
+++ result/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home'),
-    path('result_by_name', views.result_by_name, name='result_by_name'),
-    path('result', views.result, name='result')
+    path('result', views.result, name='result'),
+    path('result_by_name', views.result_by_name, name='result_by_name')
 ]
