28fb69745e952fbd531aa5b842415481eb707381
Kartik-sharma80 <49423167+Kartik-sharma80@users.noreply.github.com>
2022-08-22 13:21:10 +0530

    Update urls.py

--- result/urls.py
+++ result/urls.py
@@ -5,6 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home'),
     path('result_by_name', views.result_by_name, name='result_by_name'),
-    path('result', views.result, name='result'),
-    path('student', views.student, name='student')
+    path('result', views.result, name='result')
 ]
