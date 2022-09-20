4c2cd2559e13ff062269b70e9bc468fd39d5717e
Kartik-sharma80 <49423167+Kartik-sharma80@users.noreply.github.com>
2022-09-20 22:12:32 +0530

    Update views.py

--- result/views.py
+++ result/views.py
@@ -42,37 +42,38 @@ def result(request):
 
 def result_by_name(request):
     if request.method == "POST":
-        if request.method == "POST":
-            name = request.POST.get('name')
-            fname = request.POST.get('fname')
-            dob = request.POST.get('dob')
-            sem = request.POST.get('sem')
-            try:
-                student = Student.objects.get(name=name.upper())
-            except:
+        name = request.POST.get('name')
+        fname = request.POST.get('fname')
+        dob = request.POST.get('dob')
+        sem = request.POST.get('sem')
+        qs = Student.objects.all()
+        try:
+            student = qs.get(name=name, semester=sem)
+        except:
+            error = "yes"
+            return render(request, 'search_name.html', {'error': error})
+        else:
+            if fname.upper() != student.father_name:
                 error = "yes"
-                return render(request, 'search_name.html', {'error': error})
+                return render(request, 'search_name.html', {'error1': error})
             else:
-                if fname.upper() != student.father_name:
+                if str(dob) != str(student.dob):
                     error = "yes"
-                    return render(request, 'search_name.html', {'error1': error})
+                    return render(request, 'search_name.html', {'error2': error})
                 else:
-                    if str(dob) != str(student.dob):
+                    if sem != student.semester:
                         error = "yes"
-                        return render(request, 'search_name.html', {'error2': error})
+                        return render(request, 'search_name.html', {'error3': error})
                     else:
-                        if sem != student.semester:
-                            error = "yes"
-                            return render(request, 'search_name.html', {'error3': error})
-                        else:
-                            error = "no"
-                            total = student.sub1_marks+student.sub2_marks+student.sub3_marks+student.sub4_marks+student.sub5_marks+student.sub6_marks+student.sub7_marks
-                            pert = total//7
-                            d = {'error3': error, 'name': name, 'total': total, 'pert': pert, 'student': student}
-                            return render(request, 'result1.html', d)
+                        error = "no"
+                        total = student.sub1_marks+student.sub2_marks+student.sub3_marks+student.sub4_marks+student.sub5_marks+student.sub6_marks+student.sub7_marks
+                        pert = total//7
+                        d = {'error3': error, 'name': name, 'total': total, 'pert': pert, 'student': student}
+                        return render(request, 'result1.html', d)
     else:
         print('Get Method')
     return render(request, 'search_name.html')
+
 
 def handle_404(request, exception):
     return render(request, '404.html')
